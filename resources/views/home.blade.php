9f9eb5106cbc8a5631776ad05567050b8cd3cd3f
Nazar <thepsix007@gmail.com>
2021-12-05 21:40:55 +0200

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1687,7 +1687,7 @@
     </div>
   </div>
 
-  <div class="useful">
+  <!-- <div class="useful">
     <div class="container">
       <div class="useful__inner">
         <div class="useful__find">
@@ -1792,9 +1792,9 @@
         </div>
       </div>
     </div>
-  </div>
+  </div> -->
 
-  <!-- <div class="benefits">
+  <div class="benefits">
     <div class="container">
       <div class="benefits__inner">
         <div class="benefits__items">
@@ -1833,7 +1833,7 @@
         </div>
       </div>
     </div>
-  </div> -->
+  </div>
 
   <div class="description">
     <div class="container">
