868f6a23f76df5195d1573573e42bcedf32dd4c0
Nazar <thepsix007@gmail.com>
2021-12-05 21:47:17 +0200

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1692,7 +1692,7 @@
       <div class="useful__inner">
         <div class="useful__find">
           <div class="useful__find-subtitle">Объявления о продаже</div>
-            <img  class="js-lazy-load fade-in" data-zzload-source-img="assets/images/paw.png" data-zzload-source-srcset="assets/images/paw.webp" src='data:image/svg+xml,&lt;svg xmlns="http://www.w3.org/2000/svg" width="640" height="320"&gt;&lt;/svg&gt;' alt="#">
+            <img  class="useful__find-paw js-lazy-load fade-in" data-zzload-source-img="assets/images/paw.png" data-zzload-source-srcset="assets/images/paw.webp" src='data:image/svg+xml,&lt;svg xmlns="http://www.w3.org/2000/svg" width="640" height="320"&gt;&lt;/svg&gt;' alt="#">
           <div class="useful__find-title">Найди друга</div>
           <div class="useful__find-text">Ищете питомца? Посмотрите сотни объявлений от проверенных разводителей.</div>
           <div class="useful__find-images">
@@ -1721,7 +1721,7 @@
             </svg>
           </div>
         </div>
-        <!-- <div class="useful__materials">
+        <div class="useful__materials">
           <div class="useful__materials-inner">
             <div class="useful__materials-title">Полезные материалы</div>
             <div class="useful__materials-header">
@@ -1789,7 +1789,7 @@
               </div>
             </div>
           </div>
-        </div> -->
+        </div>
       </div>
     </div>
   </div>
